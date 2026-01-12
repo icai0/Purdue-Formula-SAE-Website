ea87b7841e3c284d87d252d71b7d0edb0a112e4f
icai0 <icaiprogramming@gmail.com>
2026-01-12 18:28:55 -0500

    redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomePage } from './home-page/home-page';
 import { TeamPage } from './team-page/team-page';
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomePage},
     {path: 'ourteam', component: TeamPage},
 ];
